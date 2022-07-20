1cf2c0042732f87d1318c9dac1e8fd70babe1642
JulaiMetel <yulichka.metel@gmail.com>
2022-07-20 14:48:43 +0200

    Fix review comments

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Немного настроим наш браузер
+# Browser settings
 driver = webdriver.Chrome(service=Service('/Users/yuliametel/PycharmProjects/OwnTestFramework/venv/lib/python3.9'
                                           '/site-packages/seleniumbase/drivers/chromedriver'),
                           options=webdriver.ChromeOptions())
 driver.maximize_window()
 driver.implicitly_wait(3)
 
-# Откроем страницу
+# Page opening
 driver.get("https://webglsamples.org/collectibles/index.html")
 try:
     elem = driver.find_element('id', 'dollbaseFemale')
