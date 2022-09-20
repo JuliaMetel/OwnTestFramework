4ac9b81e8273bc9e19be857bc0262eb1b8d97ef1
JulaiMetel <yulichka.metel@gmail.com>
2022-09-20 16:10:56 +0200

    Improve test case test_decals_head_1 for different genders

--- page_one.py
+++ page_one.py
@@ -8,11 +8,20 @@ class PageOne(FoundationPage):
     welcome_text = 'Welcome to Collectibles Painter'
     select_text = 'Select a model to begin'
 
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.genus = None
+
     def element_doll_base_female(self):
         return FoundationElement((By.ID, 'dollbaseFemale'), self._driver)
 
     def element_doll_base_male(self):
         return FoundationElement((By.ID, 'dollbaseMale'), self._driver)
+
+    def element_doll_base_male_or_female(self, genus):
+        if genus == 'male':
+            return FoundationElement((By.ID, 'dollbaseMale'), self._driver)
+        return FoundationElement((By.ID, 'dollbaseFemale'), self._driver)
 
     def element_loading_menu_welcome(self):
         return FoundationElement((By.XPATH, "//*[@id='loadingMenu']/h4"), self._driver)
--- test_gender_selection.py
+++ test_gender_selection.py
@@ -1,3 +1,4 @@
+import pytest
 from page_one import PageOne
 from page_two import PageTwo
 from utils import check_text_equal
@@ -30,15 +31,17 @@ class TestClass:
         page_one.open_page()
         check_text_equal(page_one.element_loading_menu_select().get_text(), page_one.select_text)
 
-    def test_decals_head_1_male(self, driver):
+    @pytest.mark.parametrize("genus", ['male', 'female'])
+    def test_decals_head_1(self, driver, genus):
         page_one = PageOne(driver)
         page_one.open_page()
-        page_one.element_doll_base_male().click_element()
+        page_one.element_doll_base_male_or_female(genus).click_element()
         page_two = PageTwo(driver)
         page_two.wait_page_stable()
         page_two.element_eye().click_element()
         page_two.element_head_1().click_element()
-        assert page_two.is_canvas_equal("Test_screenshots/test_decals_head_1_male.png"), "Screenshots don't match "
+        assert page_two.is_canvas_equal("Test_screenshots/test_decals_head_1_" + genus + ".png"),\
+            "Screenshots don't match"
 
 
 
