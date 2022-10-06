1a1bb8f1320d24f0a25f76ecb15c1d61245d259e
JulaiMetel <yulichka.metel@gmail.com>
2022-10-06 10:44:13 +0200

    Switching to @property

--- page_one.py
+++ page_one.py
@@ -8,14 +8,18 @@ class PageOne(FoundationPage):
     welcome_text = 'Welcome to Collectibles Painter'
     select_text = 'Select a model to begin'
 
+    @property
     def element_doll_base_female(self):
         return FoundationElement((By.ID, 'dollbaseFemale'), self._driver)
 
+    @property
     def element_doll_base_male(self):
         return FoundationElement((By.ID, 'dollbaseMale'), self._driver)
 
+    @property
     def element_loading_menu_welcome(self):
         return FoundationElement((By.XPATH, "//*[@id='loadingMenu']/h4"), self._driver)
 
+    @property
     def element_loading_menu_select(self):
         return FoundationElement((By.XPATH, "//*[@id='loadingMenu']/h2"), self._driver)
